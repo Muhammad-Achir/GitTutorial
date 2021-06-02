62f55044f4a3323f6fafbf9c3ff74e8a3e7df5c6
Muhammad Achir <muhammad.rihca@gmail.com>
2021-06-02 23:46:10 +0800

    Final Code

--- Assets/Scripts/HelloWorld.cs
+++ Assets/Scripts/HelloWorld.cs
@@ -2,17 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HelloWorld : MonoBehaviour
+public class HelloWorld
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public int health = 5;
+    public float score = 5;
 }
